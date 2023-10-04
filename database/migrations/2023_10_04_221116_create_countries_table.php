af43e030a033806fc2eb14ede5fc42dcbe2cdca6
Egor Grishin <egor-grishin@bk.ru>
2023-10-05 03:38:27 +0500

    genre && country migrations

--- database/migrations/2023_10_04_221116_create_countries_table.php
+++ database/migrations/2023_10_04_221116_create_countries_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('countries', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('name', 127);
         });
     }
 
