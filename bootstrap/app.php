8ad4955a55c1862cc08fa5512a3bf8cedfc79806
Egor Grishin <egor-grishin@bk.ru>
2023-10-05 03:40:33 +0500

    bootstrap lumen

---
+++ bootstrap/app.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+(new Laravel\Lumen\Bootstrap\LoadEnvironmentVariables(
+    dirname(__DIR__)
+))->bootstrap();
+date_default_timezone_set(env('APP_TIMEZONE', 'UTC'));
+
+$app = new Laravel\Lumen\Application(
+    dirname(__DIR__)
+);
+
+//$app->withFacades();
+//$app->withEloquent();
+
+$app->singleton(
+    Illuminate\Contracts\Debug\ExceptionHandler::class,
+    App\Exceptions\Handler::class
+);
+$app->singleton(
+    Illuminate\Contracts\Console\Kernel::class,
+    App\Console\Kernel::class
+);
+
+$app->configure('app');
+
+//$app->middleware([
+//    App\Http\Middleware\ExampleMiddleware::class
+//]);
+
+$app->register(\App\Providers\AppServiceProvider::class);
+return $app;
