7d646c3b58b1792596db0a08e3fe33877d8ebdd8
Egor Grishin <egor-grishin@bk.ru>
2023-10-05 21:10:10 +0500

    enums

---
+++ app/Enums/MenuButton.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Enums;
+
+enum MenuButton: string
+{
+    case Find = 'Найти фильм по названию';
+    case Match = 'Найти фильм на двоих';
+}
---
+++ app/Enums/State.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Enums;
+
+enum State: int
+{
+    case Menu = 1;
+    case FindMovie = 2;
+}
