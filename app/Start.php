0de54a8dee691f20f5a5356864c03c38e1770229
Egor Grishin <egor-grishin@bk.ru>
2023-10-05 23:56:35 +0500

    unblock request

--- app/Start.php
+++ app/Start.php
@@ -17,10 +17,9 @@ class Start
 {
     public function __invoke(Request $request): void
     {
-        return;
-        $this->messageIsCommand($request->dto->data)
-            ? $this->runCommandHandler($request->dto)
-            : $this->runStateHandler($request->dto);
+        if ($this->messageIsCommand($request->dto->data)) {
+            $this->runCommandHandler($request->dto);
+        }
     }
 
     private function messageIsCommand(string $message): bool
